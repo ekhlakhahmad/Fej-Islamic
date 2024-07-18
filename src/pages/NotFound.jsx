02ee7d1f27f2cf901ecd8b32c77cd25f128e392e
Ekhlakh Ahmad <ekhlakhahmad95@gmail.com>
2024-07-18 23:57:59 +0530

    not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -5,7 +5,7 @@ const NotFound = () => {
 	const navigate = useNavigate();
 	return (
 		<div className="flex flex-col justify-center items-center my-8 gap-10">
-			<h1 className="text-[100px] font-extrabold text-red-700">Oops!</h1>
+			<h1 className="text-[100px] font-extrabold text-red-800">Oops!</h1>
 			<h2 className="text-xl">404 - PAGE NOT FOUND</h2>
 			<button
 				onClick={() => navigate("/")}
