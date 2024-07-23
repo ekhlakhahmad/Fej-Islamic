9736eaf9b3070bdf2b4d2e263264125ff5f44df3
Ekhlakh Ahmad <ekhlakhahmad95@gmail.com>
2024-07-23 23:51:17 +0530

    remove not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -7,7 +7,11 @@ const NotFound = () => {
 		<div className="flex flex-col justify-center items-center my-8 gap-10">
 			<h1 className="text-[100px] font-extrabold text-red-800">Oops!</h1>
 			<h2 className="text-xl">404 - PAGE NOT FOUND</h2>
-			
+			<button
+				onClick={() => navigate("/")}
+				className="py-2 px-4 text-white bg-blue-900 hover:bg-blue-950 rounded-full">
+				Go to Home Page
+			</button>
 		</div>
 	);
 };
